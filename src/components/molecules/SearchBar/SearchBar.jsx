5934c871ae440f1e0f0b72046fc60928606682a1
Vinicius Assis <viniciusassis.dev@hotmail.com>
2022-02-23 00:52:50 -0300

    feat: add SearchContext in Home

--- src/components/molecules/SearchBar/SearchBar.jsx
+++ src/components/molecules/SearchBar/SearchBar.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Button, Flex, Input } from '@chakra-ui/react'
+import { SearchContext } from '../../../context/searchContext'
 
-const SearchBar = () => (
-  <Flex mb="50px" w="100%">
-    <Input borderTopRightRadius="0" borderBottomRightRadius="0" />
-    <Button borderTopLeftRadius="0" borderBottomLeftRadius="0">
-      Find a Book
-    </Button>
-  </Flex>
-)
+const SearchBar = () => {
+  const { searchFetch, handleChange } = useContext(SearchContext)
+
+  return (
+    <Flex mb="50px" w="100%">
+      <Input
+        onChange={handleChange}
+        borderTopRightRadius="0"
+        borderBottomRightRadius="0"
+      />
+      <Button
+        onClick={searchFetch}
+        borderTopLeftRadius="0"
+        borderBottomLeftRadius="0"
+      >
+        Find a Book
+      </Button>
+    </Flex>
+  )
+}
 
 export default SearchBar
--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,23 +1,32 @@
 import React from 'react'
 import { Flex, Grid } from '@chakra-ui/react'
 import SearchBar from '../../molecules/SearchBar/SearchBar'
-import Card from '../../molecules/Card/Card'
+import { SearchStorage } from '../../../context/searchContext'
+import CardList from '../../organisms/CardList'
 
-const Home = () => (
-  <Flex wrap="wrap" mt="40px" maxW="1280px" width="100%" px="50px" mx="auto">
-    <SearchBar />
-    <Grid
-      width="100%"
-      gap="20px"
-      gridTemplateColumns="repeat(auto-fill, minmax(240px, 1fr))"
-      justifyItems="center"
-    >
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-    </Grid>
-  </Flex>
-)
+const Home = () => {
+  return (
+    <SearchStorage>
+      <Flex
+        wrap="wrap"
+        mt="40px"
+        maxW="1280px"
+        width="100%"
+        px="50px"
+        mx="auto"
+      >
+        <SearchBar />
+        <Grid
+          width="100%"
+          gap="20px"
+          gridTemplateColumns="repeat(auto-fill, minmax(240px, 1fr))"
+          justifyItems="center"
+        >
+          <CardList />
+        </Grid>
+      </Flex>
+    </SearchStorage>
+  )
+}
 
 export default Home
